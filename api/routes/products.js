56a990ba9d2daf3fd6e0b888b1dad48cba8c4fa1
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-11-17 23:03:25 +0530

    fix run time error

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,6 +9,8 @@ router.get('/',(req, res, next) => {
 
 router.post('/',(req, res, next) => {
     res.status(200).json({
-        message:'Handling GET requests to /products'
+        message:'Handling POST requests to /products'
     });
 });
+
+module.exports =router;
