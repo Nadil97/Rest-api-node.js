57112ea6aee6653e3cdfa140c0d813d89cb83b65
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-11-17 14:20:44 +0530

    start server

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express =require('express');
 const app =express();
 app.use((req, res, next) =>{
-
+    res.status(200).json({
+        message: 'It work!'
+    });
 });
+
+module.exports =app;
